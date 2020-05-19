3764cfd4d0f917e69e68378ac8787b12d1ddd440
葉月 薮 <h1201919014@hiratagakuen.onmicrosoft.com>
2020-05-19 12:40:28 +0900

    タイトルObjとシーンの追加(仮)

---
+++ Project1/Project1/素材/ObjTitle.h
@@ -0,0 +1,20 @@
+#pragma once
+//使用するヘッダー
+#include"GameL/SceneObjManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+
+//オブジェクト：ステージセレクト
+class CObjTemp : public CObj
+{
+public:
+	CObjTemp() {};
+	~CObjTemp() {};
+	void Init();	//イニシャライズ
+	void Action();	//アクション
+	void Draw();	//ドロー
+	float x, y;
+private:
+	int hoge;//仮
+};
