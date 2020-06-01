512edfaa305c0dc2442bb8cca54f393ef942b02a
紫苑 岩本 <h1201916004@hiratagakuen.onmicrosoft.com>
2020-06-01 14:10:21 +0900

    Pose表示

--- Project1/Project1/SceneGameMain.cpp
+++ Project1/Project1/SceneGameMain.cpp
@@ -63,6 +63,7 @@ void CSceneGameMain::InitScene()
 	Draw::LoadImageW(L"素材/仮/image.png", 0, TEX_SIZE_2048);
 	Draw::LoadImageW(L"素材/仮/mapcip.png", 1, TEX_SIZE_1024);
 	Draw::LoadImageW(L"素材/仮/Animation.png", 2, TEX_SIZE_64);
+	Draw::LoadImageW(L"素材/仮/Pose.png", 3, TEX_SIZE_64);
 	//Audio::Start(0);
 
 	//主人公オブジェクト作成
@@ -72,6 +73,10 @@ void CSceneGameMain::InitScene()
 	//stageオブジェクト作成
 	CObjStage* objb = new CObjStage(map);
 	Objs::InsertObj(objb, OBJ_STAGE, 9);
+
+	//Poseオブジェクト作成
+	CObjPose* objp = new CObjPose(0,0);
+	Objs::InsertObj(objp, OBJ_POSE, 11);
 
 
 }
